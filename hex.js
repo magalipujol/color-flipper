3ec35a6b406a50a26b5461a9a659b551a2bf8ab1
magalipujol <magalipuuujol@gmail.com>
2021-05-04 13:19:55 -0300

    hex part finished

--- hex.js
+++ hex.js
@@ -1,14 +1,28 @@
-const hexPossibleValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"] 
-//TODO
-function createHexColorFromValues(red, green, blue) {
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color")
 
+const hexPossibleValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"] 
+//TODO 
+let hexString = "#"
+function createRandomHexColor() {
+    hexString = "#"
+    for (let i = 0; i < 6; i++) {
+        hexString += hexPossibleValues[getRandomNumberUpToANumber(hexPossibleValues.length - 1)]
+    }
+    return hexString
 }
+
+
+
  
-//TODO change from rgb to hex
+function getRandomNumberUpToANumber(biggest) {
+    return Math.floor(Math.random() * (biggest + 1))
+}
+
 btn.addEventListener("click", function (){
-    document.body.style.backgroundColor = createRGBColorFromValues(getRandomNumberUpToANumber(255), 
-                                                                   getRandomNumberUpToANumber(255), 
-                                                                   getRandomNumberUpToANumber(255))
-    color.textContent = `RGB(${redGlobal}, ${greenGlobal}, ${blueGlobal})`
+    document.body.style.backgroundColor = createRandomHexColor()
+    color.textContent = hexString
 })
+
+
 
